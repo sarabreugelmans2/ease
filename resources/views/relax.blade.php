634aae6f5de4f1ae0c01b8427c96b1c7ac5f658b
Sara <sara_breugelmans@hotmail.com>
2018-10-07 16:56:06 +0200

    layout met yields

--- resources/views/relax.blade.php
+++ resources/views/relax.blade.php
@@ -1,16 +1,21 @@
-<h1>Hooray, you completed your daily session.</h1>
-<h2>How are you feeling now? </h2>
+@extends('layouts/app')
+@section('title')
+Track relax
+@endsection
+@section('content')
+    <h1>Hooray, you completed your daily session.</h1>
+    <h2>How are you feeling now? </h2>
 
-<div class="relax">
-    <form action="" method="post" id="uploadForm">
-        <input type="radio" value="less" name="status">
-        <label for="less"> Less relaxed </label>
-        <input type="radio" value="same" name="status">
-        <label for="same">Same as before my relaxation habbit </label>
-        <input type="radio" value="more" name="status">
-        <label for="more"> More relaxed </label> 
+    <div class="relax">
+        <form action="" method="post" id="uploadForm">
+            <input type="radio" value="less" name="status">
+            <label for="less"> Less relaxed </label>
+            <input type="radio" value="same" name="status">
+            <label for="same">Same as before my relaxation habbit </label>
+            <input type="radio" value="more" name="status">
+            <label for="more"> More relaxed </label> 
 
-        <input type="submit" value="Submit" name="submit" class="button">
-    </form>    
-</div>
-
+            <input type="submit" value="Submit" name="submit" class="button">
+        </form>    
+    </div>
+@endsection
