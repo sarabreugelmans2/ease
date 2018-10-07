9e0e2d5f35b11551fcfa9f15c690bf1322cfc27f
Sara <sara_breugelmans@hotmail.com>
2018-10-07 17:58:08 +0200

    added habits to db

--- app/Habit.php
+++ app/Habit.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Habit extends Model
 {
-   
     public function episodes()
     {
         return $this->hasMany('App\Episode');
--- database/seeds/HabitSeeder.php
+++ database/seeds/HabitSeeder.php
@@ -11,6 +11,12 @@ class HabitSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = array(
+            array('name'=>'Breathing', 'description'=> 'Breathe in and out, and in and out, and in ...'),
+            array('name'=>'Walking', 'description'=> 'Go outside and take a walk'),
+            array('name'=>'Stargazing', 'description'=> 'Gaze at the stars, get lost in the mistery of the universe'),
+            array('name'=>'Cloudwatching', 'description'=> 'Watch the clouds')
+        );
+        $habit= \App\Habit::insert($data);
     }
 }
