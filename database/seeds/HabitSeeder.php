6e8d289a60922d8437a9e421048979e9421d0cba
Kobo-one <kobe.christiaensen@hotmail.com>
2018-10-12 17:59:38 +0200

    New arrays, and extra habbit

--- database/seeds/HabitSeeder.php
+++ database/seeds/HabitSeeder.php
@@ -11,12 +11,13 @@ class HabitSeeder extends Seeder
      */
     public function run()
     {
-        $data = array(
-            array('name'=>'Breathing', 'description'=> 'Breathe in and out, and in and out, and in ...'),
-            array('name'=>'Walking', 'description'=> 'Go outside and take a walk'),
-            array('name'=>'Stargazing', 'description'=> 'Gaze at the stars, get lost in the mistery of the universe'),
-            array('name'=>'Cloudwatching', 'description'=> 'Watch the clouds')
-        );
+        $data = [
+            ['name'=>'Breathing', 'description'=> 'Breathe in and out, and in and out, and in ...'],
+            ['name'=>'Walking', 'description'=> 'Go outside and take a walk'],
+            ['name'=>'Stargazing', 'description'=> 'Gaze at the stars, get lost in the mistery of the universe'],
+            ['name'=>'Cloudwatching', 'description'=> 'Watch the clouds, find the story your maker wants to tell you'],
+            ['name'=>'Running', 'description'=> 'Go for a run to clear your mind']
+        ];
         $habit= \App\Habit::insert($data);
     }
 }
