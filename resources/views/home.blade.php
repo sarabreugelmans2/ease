aebaca34c3794c70611699daad48908005712614
thecoderiver <hitman49.vanoerle@gmail.com>
2018-10-12 16:48:36 +0200

    Added sass component file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts/app')
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
+<div id="ActiveHabit"></div>
+
+<div class="subhabit">
+    <div class="subhabit-item"></div>
+    <div class="subhabit-item"></div>
+    <div class="subhabit-item"></div>
+</div>
+
+<div class="progressBar">
+    <div class="progressBar--progress"></div>
+</div>
 
 @section('content')
     @component('components/navigation')
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/navigation.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/navigation.scss', 'public/css');
+    .sass('resources/sass/components/navigation.scss', 'public/css')
+    .sass('resources/sass/home.scss', 'public/css');
