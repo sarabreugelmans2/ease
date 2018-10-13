3b6a35b018e7fe912a9977fb65f00b475a029f88
thecoderiver <hitman49.vanoerle@gmail.com>
2018-10-13 15:51:57 +0200

    Finished design for the interests view

--- resources/views/interests.blade.php
+++ resources/views/interests.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts/app')
 <link rel="stylesheet" href="{{ asset('css/interests.css') }}">
-<p>Select what you like (At least 4)</p>
+<link rel="stylesheet" href="{{ asset('css/buttons.css') }}">
+
+<p class="instructions">Select what you like (At least 4)</p>
 
 <div class="habits">
     <div class="habit">
@@ -38,3 +40,7 @@
         <p>breathing</p>
     </div>
 </div>
+
+<div class="CTA_bg">
+    <a class="btn--CTA" href="#">done</a>  
+</diV>
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,6 @@ mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/navigation.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/components/navigation.scss', 'public/css')
+    .sass('resources/sass/components/buttons.scss', 'public/css')
     .sass('resources/sass/home.scss', 'public/css')
     .sass('resources/sass/interests.scss', 'public/css');
