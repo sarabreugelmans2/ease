14b1b7eedf3696c98abd7513a7fb9f0110428632
thecoderiver <hitman49.vanoerle@gmail.com>
2018-10-14 15:43:41 +0200

    Created design for dashboard stats

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts/app')
 <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
 
+{{-- header menu --}}
+
 <div class="menu">
     <div class="selector"></div>
     <div class="menuOptions--option selected">
@@ -10,6 +12,49 @@
         <a href="">calender</a>
     </div>
 </div>
+
+{{-- end header menu --}}
+<div class="stats_wrapper">
+    <div class="habit">
+        <p class="habit--name">Breathing exercises</p>
+        <div class="habit--progress-wrapper">
+            <div class="habit--progress-fill">
+                <p class="habit--progress-number">10%</p>
+            </div>
+        </div>
+        <div class="habit--levels-wrapper">
+            <p class="habit--levels-begin">Level 3 - panda</p>
+            <p class="habit--levels-end">Level 4</p>
+        </div>
+    </div>
+
+    <div class="habit">
+        <p class="habit--name">Breathing exercises</p>
+        <div class="habit--progress-wrapper">
+            <div class="habit--progress-fill">
+                <p class="habit--progress-number">10%</p>
+            </div>
+        </div>
+        <div class="habit--levels-wrapper">
+            <p class="habit--levels-begin">Level 3 - panda</p>
+            <p class="habit--levels-end">Level 4</p>
+        </div>
+    </div>
+
+    <div class="habit">
+        <p class="habit--name">Breathing exercises</p>
+        <div class="habit--progress-wrapper">
+            <div class="habit--progress-fill">
+                <p class="habit--progress-number">10%</p>
+            </div>
+        </div>
+        <div class="habit--levels-wrapper">
+            <p class="habit--levels-begin">Level 3 - panda</p>
+            <p class="habit--levels-end">Level 4</p>
+        </div>
+    </div>
+</div>
+
 
 @section('content')
     @component('components/navigation')
