c7f85e425c8b32ff2071917187d2e3cb2541cb01
thecoderiver <hitman49.vanoerle@gmail.com>
2018-10-14 14:42:52 +0200

    Created header menu for dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts/app')
 <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
 
+<div class="menu">
+    <div class="selector"></div>
+    <div class="menuOptions--option selected">
+        <a href="">level</a>
+    </div>
+    <div class="menuOptions--option">
+        <a href="">calender</a>
+    </div>
+</div>
+
 @section('content')
     @component('components/navigation')
     @endcomponent
