5acf1c26a3cb318a43cc6cd3e7cdc795711e3a74
Sara <sara_breugelmans@hotmail.com>
2018-10-18 14:17:26 +0200

    pie charts for user opinions on habits

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Charts\UserOpinions;
+
+
 
 class AdminController extends Controller
 {
@@ -11,9 +14,36 @@ class AdminController extends Controller
         // $data['less']=\App\Relax::where('status',0);
         // $data['same']=\App\Relax::where('status',1);
         // $data['more']=\App\Relax::where('status',2);
-        $data['activity']=\App\Relax::where('user_id', $userId)->orderBy('endTime', 'desc')->get();
+        //$data['activity']=\App\Relax::where('user_id', $userId)->orderBy('endTime', 'desc')->get();
+        $habit1=[];
+        $habit2=[];
+        $habit3=[];
+        $habit4=[];
+        $habit5=[];
+        $habit6=[];
+    for($x=1; $x<=6; $x++){
+       
+        for($i=0; $i<=2; $i++){
+           $h =\App\Relax::where([['status','=',$i],['habit_id','=',$x]])->count();
+          
+           array_push(${'habit'.$x}, $h);
+           $name=\App\Habit::select('name')->where('id','=',$x)->first();
+           $name=$name['name'];
+        };
 
-        return view('calendar', $data);
-        return view('admin');
+        ${'chart'.$x} = new UserOpinions;
+        ${'chart'.$x}->labels(['Bad', 'Neutral', 'Awesome']);
+        ${'chart'.$x}->displayAxes(false)->options([
+            'title' => [
+                'text' => $name,
+                'display' => true // or false, depending on what you want.
+            ]
+        ]);
+        
+        ${'chart'.$x}->dataset('Useropinions', 'doughnut', ${'habit'.$x})->backgroundColor(['#B6B6B7','#4B667F',"#C5E6EE"]);
+    }
+    
+   
+        return view('admin', compact('chart1', 'chart2', 'chart3', 'chart4', 'chart5', 'chart6' ));
     }
 }
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,3 +1,5 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
+
 @extends('layouts/app')
 @section('title')
 Admin dashboard
@@ -5,6 +7,15 @@ Admin dashboard
 
 @section('content')
     <h1> Admin </h1>
-
+  @for($i=1; $i<=6;$i++)
+    <div>
+        <h1>
+        </h1>
+    {!! ${'chart'.$i}->container() !!}
+    {!! ${'chart'.$i}->script() !!}
+    </div>
+   @endfor
+    
+   
   
 @endsection
