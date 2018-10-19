ba8bcbaeb96f01ac56048982986dfcd0bedb1726
Kobo-one <kobe.christiaensen@hotmail.com>
2018-10-19 15:38:17 +0200

    User habits get posted to the database

--- app/Http/Controllers/InterestsController.php
+++ app/Http/Controllers/InterestsController.php
@@ -4,10 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Auth;
+
+use App\User_habit;
+
 class InterestsController extends Controller
 {
     public function index(){
         $data['habits'] = \DB::table('habits')->get();
         return view('interests', $data);
+    }
+
+    public function save(Request $request){
+        $this->validate($request, [
+            'habits' => 'required'
+        ]);
+        $habits = $request->habits;
+        $habit_count = count($habits);
+        $userId =Auth::user()->id;
+        
+        if($habit_count < 4){
+            return back()->withError('You have only selected '.$habit_count.' of 4 habits')  ;      
+        }
+
+        \App\user_habit::where('user_id', $userId)->delete();
+        foreach($habits as $habitId){
+            $habit = new User_habit;
+            $habit->user_id = $userId;
+            $habit->habit_id = $habitId;
+            $habit->save();
+        }
+
+        return redirect('/');
     }
 }
--- database/migrations/2018_10_18_183830_create_user_habits_table.php
+++ database/migrations/2018_10_18_183830_create_user_habits_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserHabitTable extends Migration
+class CreateUserHabitsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUserHabitTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_habit', function (Blueprint $table) {
+        Schema::create('user_habits', function (Blueprint $table) {
             $table->increments('id');
 
             $table->integer('user_id')->unsigned();;
@@ -31,6 +31,6 @@ class CreateUserHabitTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_habit');
+        Schema::dropIfExists('user_habits');
     }
 }
