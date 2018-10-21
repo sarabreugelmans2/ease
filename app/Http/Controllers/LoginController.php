0bcbfae99e6d839bb30a5d7fe9a409e189dc3596
thecoderiver <hitman49.vanoerle@gmail.com>
2018-10-21 13:05:02 +0200

    builded and styled habit overview

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Classes\UserStatsClass;
 
 class LoginController extends Controller
 {
     public function login()
-    {
-        
-        return view('login');
-
+    {   
+        $UserStatsClass = new UserStatsClass();
+        $userstats = $UserStatsClass->getTrackedHabitsCount();
+        return view('login', $userstats);
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,6 +8,32 @@
     <div class="wave"></div>
 </div>
 
+<div class="statsWrapper">
+    <div class="statsWrapper--userStat">
+        <p class="statsWrapper--userStat--title">Breathing exercises</p> 
+        <p class="statsWrapper--userStat--value">{{ $BreathingCount }}</p>
+    </div>
+    <div class="statsWrapper--userStat">
+        <p class="statsWrapper--userStat--title">Walks taken</p> 
+        <p class="statsWrapper--userStat--value">{{ $WalkingCount }}</p>
+    </div>
+    <div class="statsWrapper--userStat">
+        <p class="statsWrapper--userStat--title">Stars counted</p> 
+        <p class="statsWrapper--userStat--value">{{ $StargazingCount }}</p>
+    </div>
+    <div class="statsWrapper--userStat">
+        <p class="statsWrapper--userStat--title">Clouds watched</p> 
+        <p class="statsWrapper--userStat--value">{{ $CloudwatchingCount }}</p>
+    </div>
+    <div class="statsWrapper--userStat">
+        <p class="statsWrapper--userStat--title">runs taken</p> 
+        <p class="statsWrapper--userStat--value">{{ $RunningCount }}</p>
+    </div>
+    <div class="statsWrapper--userStat">
+        <p class="statsWrapper--userStat--title">outside time</p> 
+        <p class="statsWrapper--userStat--value">{{ $OutsideCount }}</p>
+    </div>
+</div>
 <div class="CTA_bg">
    <a class="btn--CTA" href="https://www.strava.com/oauth/authorize?client_id=29145&redirect_uri=http://homestead.test/loggedin&response_type=code">Let's get started!</a>
 </diV>
